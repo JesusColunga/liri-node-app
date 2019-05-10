7bb96b93feb6580cab84197dfcabd7fc6e4c3ead
Jesus Colunga <jesus.colunga@proapi.com.mx>
2019-05-09 19:48:19 -0500

    Packages installed

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # liri-node-app
-Language Interpretation and Recognition Interface
+Language Interpretation and Recognition Interface (LIRI Bot),
+
+ a command line Node JS app  that takes in parameters and gives you back data.
+
---
+++ liri.js
@@ -0,0 +1,44 @@
+// liri.js
+// 9/May/2019
+// LIRI Bot: Language Interpretation and Recognition Interface
+
+
+// GLOBAL VARIABLES
+// =======================================================================================
+var axios = require("axios");
+
+
+// OBJECTS
+// =======================================================================================
+
+
+// FUNCTIONS (Definition)
+// =======================================================================================
+
+
+// FUNCTION CALLS (Execution)
+// =======================================================================================
+
+
+// Creation process
+// =======================================================================================
+/*
+npm init
+
+npm i axios
+https://www.npmjs.com/package/axios
+
+npm i node-spotify-api
+https://www.npmjs.com/package/node-spotify-api
+
+http://www.omdbapi.com/
+
+http://www.artists.bandsintown.com/bandsintown-api
+
+npm i moment
+https://www.npmjs.com/package/moment
+
+npm i dotenv
+https://www.npmjs.com/package/dotenv
+
+*/
