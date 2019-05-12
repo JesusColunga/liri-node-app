19ad2c252e02f5ff487ebd388c961d2cd07198f5
Jesus Colunga <jesus.colunga@proapi.com.mx>
2019-05-11 20:36:29 -0500

    Log file, checking

--- liri.js
+++ liri.js
@@ -15,26 +15,14 @@ var argArr  = process.argv.slice(3);
 var params  = argArr.join (" ");
 var fs = require("fs");
 var moment = require ("moment");
+var stringResult = "";
 
 
 // FUNCTIONS (Definition)
 // =======================================================================================
+//----------------------------------   Utility functions
 function writeKeys (data) {
     var dataSt = JSON.stringify( Object.keys(data) ).replace(/,/g, "\n");
-    fs.writeFile("log.txt", 
-                 dataSt, 
-                 function(err) {
-                    if (err) {
-                        return console.log("Error writing the log file:", err);
-                    }
-                    console.log("Log file created.");
-                 }
-                );
-};
-
-function writeLog (data) {
-    var dataSt = JSON.stringify(data);
-    //console.log ("JSON:", dataSt);
     fs.writeFile("log.txt", 
                  dataSt, 
                  function(err) {
@@ -51,12 +39,41 @@ function showKeys (resp) {
     console.log ("Keys:", Object.keys(resp));
 };
 
+//----------------------------------------------------------
+function writeLog (strg, showMsg) {
+    fs.appendFile("log.txt", 
+                  strg, 
+                  function(err) {
+                    if (err) {
+                        return console.log("Error writing the log file:", err);
+                    };
+                    if (showMsg) {
+                        console.log("Log file updated.");
+                    };
+                  }
+                 );
+};
+
+function showConsoleWriteFile (strg) {
+    strg = "\n========================================" +
+           "\nCommand:    " + command                   +
+           "\nParameters: " + params                    +
+           "\n\n"           + strg    + "\n\n";
+    console.log(strg);
+    writeLog (strg, true);
+};
+
 function unknownCommand () {
-    console.log("\n'" + command + "' is not recognized as a valid command for LIRI.");
+    showConsoleWriteFile(
+        "'" + command + "' is not recognized as a valid command for LIRI."
+    );
 };
 
 //----------------------------------------------------------
 function doWhatExReadFile (){
+    showConsoleWriteFile(
+        "   Execution of the command located in file  - - ->"
+    );
     fs.readFile("random.txt", 
                 "utf8", 
                 function(error, data) {
@@ -79,7 +96,8 @@ function doWhatExReadFile (){
                         params = "";
                     };
 
-                    console.log("   - - - -");
+                    //console.log("\n   - - - -\n");
+                    //writeLog("\n   - - - -\n", false);
                     identifyCommand ();
                 });
 };
@@ -101,17 +119,17 @@ function movieExRating (ratings){
 };
 
 function movieShowInfo (info) {
-    console.log (
-        "\n   Title of the movie:                   ", info.Title,
-        "\n   Year the movie came out:              ", info.Year,
-        "\n   IMDB Rating of the movie:             ", info.imdbRating,
-        "\n   Rotten Tomatoes Rating of the movie:  ", movieExRating (info.Ratings),
-        "\n   Country where the movie was produced: ", info.Country,
-        "\n   Language of the movie:                ", info.Language, "\n",
-        "\n   Plot of the movie:                    ",
-        "\n", info.Plot, "\n",
-        "\n   Actors in the movie:                  ",
-        "\n", info.Actors, "\n"
+    showConsoleWriteFile (
+        "\n   Title of the movie:                   " + info.Title +
+        "\n   Year the movie came out:              " + info.Year +
+        "\n   IMDB Rating of the movie:             " + info.imdbRating +
+        "\n   Rotten Tomatoes Rating of the movie:  " + movieExRating (info.Ratings) +
+        "\n   Country where the movie was produced: " + info.Country +
+        "\n   Language of the movie:                " + info.Language + "\n" +
+        "\n   Plot of the movie:                    " +
+        "\n" + info.Plot + "\n" +
+        "\n   Actors in the movie:                  " +
+        "\n" + info.Actors + "\n"
     );
 };
 
@@ -147,17 +165,16 @@ function spotifyShowInfo (item, index) {
     var artSt = "";
     item.artists
     .forEach(
-       function(value, index){
-           artSt === "" ? artSt = value.name : artSt += ", " + value.name;
-       }
+    function(value, index){
+        artSt === "" ? artSt = value.name : artSt += ", " + value.name;
+    }
     );
-    console.log (
-        "\n----- ", index, " -----",
-        "\n   Artist:      ", artSt,
-        "\n   Song's name: ", item.name,
-        "\n   Link:        ", item.preview_url,
-        "\n   Album:       ", item.album.name
-    );
+    stringResult +=
+        "\n----- " + index + " -----" +
+        "\n   Artist:      " + artSt +
+        "\n   Song's name: " + item.name +
+        "\n   Link:        " + item.preview_url +
+        "\n   Album:       " + item.album.name;
 };
 
 function spotifyExError(error) {
@@ -165,7 +182,9 @@ function spotifyExError(error) {
 };
 
 function spotifyExOk(response) {
+    stringResult = "";
     response.tracks.items.forEach( spotifyShowInfo  );
+    showConsoleWriteFile (stringResult);
 };
 
 function spotifyExec () {
@@ -183,12 +202,11 @@ function spotifyExec () {
 
 //                 ----------
 function concertShowInfo (item, index) {
-    console.log (
-        "\n----- ", index, " -----",
-        "\n   Name of the venue:", item.venue.name,
-        "\n   Venue location:   ", item.venue.country + ", " + item.venue.city,
-        "\n   Date of the event:", moment(item.venue.datetime).format("MM/DD/YYYY")
-    );
+    stringResult += 
+        "\n----- " + index + " -----" +
+        "\n   Name of the venue:" + item.venue.name    +
+        "\n   Venue location:   " + item.venue.country + ", " + item.venue.city +
+        "\n   Date of the event:" + moment(item.venue.datetime).format("MM/DD/YYYY");
 };
 
 function concertExError(error) {
@@ -196,7 +214,9 @@ function concertExError(error) {
 };
 
 function concertExOk(response) {
+    stringResult = "";
     response.data.forEach(concertShowInfo);
+    showConsoleWriteFile (stringResult);
 };
 
 function concertExec () {
@@ -215,8 +235,6 @@ function concertExec () {
 //----------------------------------------------------------
 
 function identifyCommand () {
-    console.log ('Command:'   , command);
-    console.log ('Parameters:', params );
     var comm = command.toLowerCase();
     if (comm === "concert-this"     ) { concertExec(); } else
     if (comm === "spotify-this-song") { spotifyExec(); } else
